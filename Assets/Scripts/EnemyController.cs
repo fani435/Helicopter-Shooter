79c037881530389f0089070cf9c9fa0bf9d6af83
fani435 <farhansarwar435@gmail.com>
2022-05-22 13:33:00 +0500

    Shooting implemented

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float Health;
+    public float RespawnTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.CompareTag("Bullet"))
+        {
+            //Hiding the bullet since we have been hit
+            collision.transform.gameObject.SetActive(false);
+            TakeDamage();
+        }
+    }
+
+    public void TakeDamage()
+    {
+        Health -= 1;
+
+        if (Health == 0)
+        {
+            //Hiding the visible red cube
+            GetComponent<MeshRenderer>().enabled = false;
+            //Disabling the collider
+            GetComponent<BoxCollider>().enabled = false;
+            //Starting this coroutine so enemy can respawn after some fixed time
+            StartCoroutine(RespawnAfterDelay());
+        }
+    }
+
+    IEnumerator RespawnAfterDelay()
+    {
+        yield return new WaitForSeconds(RespawnTime);
+        //Restoring health of enemy
+        Health = 3f;
+        //Showing the enemy again
+        GetComponent<MeshRenderer>().enabled = true;
+        //Enabling the collider
+        GetComponent<BoxCollider>().enabled = true;
+    }
+
+}
---
+++ Assets/Scripts/ShootingManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingManager : MonoBehaviour
+{
+    public GameObject Bullet;
+    public float BulletSpeed;
+    public float TimeBetweenBullets;
+
+    public bool CanShoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Can helicopter shoot yet
+        if(CanShoot)
+        {
+            //If the object in range is an enemy
+            if (other.CompareTag("Enemy"))
+            {
+                Debug.Log("Found enemy");
+                //Preventing helicopter from shooting again until TimeBetweenBullets has passed
+                CanShoot = false;
+                Shoot(other.transform);
+            }
+        }
+    }
+
+    public void Shoot(Transform Target)
+    {
+        //Moving bullet to the barrel of the gun
+        Bullet.transform.position = transform.position;
+        //Point bullet to target
+        Bullet.transform.LookAt(Target);
+        //Show bullet
+        Bullet.SetActive(true);
+        //Shoot bullet
+        Bullet.GetComponent<Rigidbody>().velocity = Bullet.transform.forward * BulletSpeed;
+        //Wait for a moment before shooting again
+        StartCoroutine(WaitForTimeBetweenBullets());
+    }
+
+    IEnumerator WaitForTimeBetweenBullets()
+    {
+        yield return new WaitForSeconds(TimeBetweenBullets);
+        CanShoot = true;
+    }
+}
